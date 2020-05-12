7a90c2cc2bfcd4bf8102467423480af7f4c818c4
3xter99 <60340614+3xter99@users.noreply.github.com>
2020-05-12 18:27:06 +0300

    Учусь. ФотоПачка

---
+++ test.js
@@ -0,0 +1,49 @@
+const img = [
+    'autumn-219972_1280.jpg',
+    'california-1751455_1280.webp',
+    'fantasy-3077928_1280.webp',
+    'feather-3010848_1280.webp',
+    'pexels-photo-1402787.jpeg',
+    'pexels-photo-853199.jpeg'
+];
+
+let count = img.length;
+const cover = document.querySelector('.cover')
+
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+}
+
+function randInt(min, max) {
+    // случайное число от min до (max+1)
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
+
+function init () {
+    shuffle(img);
+    for (let i = 0; i < img.length; i++) {
+        let card = document.createElement('div');
+        card.classList.add('card');
+        card.style.background = `url('./images/${img[i]}')`;
+        card.style.backgroundSize = 'cover';
+        card.style.transform = `rotate(${randInt(-15, 15)}deg) translate(${randInt(-40, 40)}px, ${randInt(-40, 40)}px)`;
+        cover.append(card);
+    }
+}
+
+init();
+
+cover.addEventListener('click', event => {
+    if (event.target.classList.contains('card')) {
+        event.target.classList.add('go');
+        count--;
+    }
+    if (count == 0) {
+        /**
+         * удалить все card
+         */
+        init();
+        count = img.length;
+    }
+});
